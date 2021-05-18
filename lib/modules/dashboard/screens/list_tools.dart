e7bc9d1432cf61ede65f6fa796ece700e12442d1
matheus1714 <matheussilvamartins1714@gmail.com>
2021-05-18 14:16:47 -0300

    create list cards

---
+++ lib/modules/dashboard/screens/list_tools.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../../../widgets/backgound_screen.dart';
+import '../../../constants/colors_defaults.dart';
+import '../widgets/card_tools.dart';
+import '../../../utils/helper/get_grid_type_card.dart';
+import '../../../utils/helper/adjust_icon_dimension.dart';
+
+class ListTools extends StatefulWidget {
+  @override
+  _ListToolsState createState() => _ListToolsState();
+}
+
+class _ListToolsState extends State<ListTools> {
+  double _widthScreen;
+  double _heightScreen;
+  int _gridType;
+  double _sizeIcon;
+
+  void setSizeIcon(double w, double h) {
+    this.setState(() {
+      _sizeIcon = adjustIconDimension(w, h);
+    });
+  }
+
+  void setGridType(double w, double h) {
+    this.setState(() {
+      _gridType = getGridTypeCard(w, h);
+    });
+  }
+
+  void setLengthScreen(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    this.setState(() {
+      _widthScreen = w;
+      _heightScreen = h;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    setLengthScreen(context);
+    setGridType(_widthScreen, _heightScreen);
+    setSizeIcon(_widthScreen, _heightScreen);
+    return Scaffold(
+      body: Container(
+        decoration: backgroundScreen(
+            DefaultColors.whiteColor, DefaultColors.secondaryVioletColor),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              cardTools(_widthScreen, _heightScreen, _gridType, _sizeIcon),
+              cardTools(_widthScreen, _heightScreen, _gridType, _sizeIcon),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
