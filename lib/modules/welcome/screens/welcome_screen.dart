0641449800c7b701a11d88224c9699612da96218
matheus1714 <matheussilvamartins1714@gmail.com>
2021-05-17 21:13:49 -0300

    welcome screen format text

--- lib/modules/welcome/screens/welcome_screen.dart
+++ lib/modules/welcome/screens/welcome_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mytools/constants/colors_defaults.dart';
+import 'package:mytools/widgets/backgound_screen.dart';
+import 'package:mytools/modules/welcome/widgets/welcome_text.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome'),
-          ],
+      body: Container(
+        decoration: backgroundScreen(
+            DefaultColors.whiteColor, DefaultColors.secondaryVioletColor),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              welcomeText('Welcome'),
+              welcomeText('To'),
+              welcomeText('MyTools'),
+            ],
+          ),
         ),
       ),
     );
