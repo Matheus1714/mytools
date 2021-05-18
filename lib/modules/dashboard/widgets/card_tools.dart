1f07d9347a1cfbe4b34fc6c82586e7e4f690bcb0
matheus1714 <matheussilvamartins1714@gmail.com>
2021-05-18 19:48:11 -0300

    adjust card for clean code

--- lib/modules/dashboard/widgets/card_tools.dart
+++ lib/modules/dashboard/widgets/card_tools.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../../constants/colors_defaults.dart';
 import '../../../utils/helper/lengths_card.dart';
+import './icon_tools.dart';
+import './text_box_card.dart';
 
 Card cardTools(double w, double h, int gridType, double sizeIcon) {
   double cardWidth;
@@ -14,57 +16,19 @@ Card cardTools(double w, double h, int gridType, double sizeIcon) {
 
   return Card(
     color: DefaultColors.secondaryVioletColor,
-    child: InkWell(
-      splashColor: DefaultColors.primaryVioletColor,
-      onTap: () {},
-      child: SizedBox(
-        width: cardWidth,
-        height: cardHeight,
-        child: GridView.count(
-          crossAxisCount: gridType,
-          controller: new ScrollController(keepScrollOffset: false),
-          shrinkWrap: true,
-          scrollDirection: Axis.vertical,
-          childAspectRatio: 1.8,
-          children: [
-            Container(
-              child: Center(
-                child: Icon(
-                  Icons.favorite,
-                  size: 140 * sizeIcon,
-                  color: DefaultColors.primaryYellowColor,
-                ),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: DefaultColors.primaryVioletColor,
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  children: [
-                    SizedBox(height: 10),
-                    Text(
-                      'Example',
-                      style: TextStyle(
-                        color: DefaultColors.whiteColor,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'ldkfjsldkfjsldkfj sdfl ksdjflsdj flksdjflksdj flsdj f',
-                      style: TextStyle(
-                        color: DefaultColors.whiteColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+    child: SizedBox(
+      width: cardWidth,
+      height: cardHeight,
+      child: GridView.count(
+        crossAxisCount: gridType,
+        controller: new ScrollController(keepScrollOffset: false),
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        childAspectRatio: 1.8,
+        children: [
+          iconTools(sizeIcon),
+          textBoxCard()
+        ],
       ),
     ),
   );
